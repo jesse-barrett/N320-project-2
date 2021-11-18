bf101d63004c96d1a512a0776cd28a2936635c81
JesseB) <jeslbarr@iu.edu>
2021-11-18 15:41:36 -0500

    Implemented isLoaded state
    It allows our data to be displayed without using temporary empty datasets in out state.

--- src/components/villagers.js
+++ src/components/villagers.js
@@ -1,47 +1,48 @@
+import { render } from "@testing-library/react";
 import React, { useEffect, useState } from "react";
 
 export default function Villager() {
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
   const [villagers, setVillagers] = useState([]);
-  const [currentVillager, setCurrentVillager] = useState([]);
+  // const [currentVillager, setCurrentVillager] = useState([]);
   const [index, setIndex] = useState(0);
 
   //retrieve the data
   useEffect(() => {
     fetch("data/villagers.json")
       .then((response) => response.json())
-      .then((data) => {
-        //save the data
-        setVillagers(data.Villagers);
-        // console.log(villagers[index].name);
-      });
+      .then(
+        (data) => {
+          //save the data
+          setVillagers(data.Villagers);
+          setIsLoaded(true);
+        },
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      );
   }, []);
 
-  let bio = villagers.map((villager) => {
-    //so basically no
-  });
-
-  // let villagerList = villagers.map((villager) => {
-  //   let servicesList = villager.services.map((services, ind) => (
-  //     <div key={ind}>{services}</div>
-  //   ));
-
-  //   return (
-  //     <li key={villager.id}>
-  //       <div className="title">{villager.name}</div>
-  //       <div className="body">Gender: {villager.gender}</div>
-  //       <div className="body">Species: {villager.species}</div>
-  //       <div className="body">Birthday: {villager.birthday}</div>
-  //       <div className="body">Astrology: {villager.astrology}</div>
-  //       <div className="body">Services: {servicesList}</div>
-  //     </li>
-  //   );
-  // });
-
-  return (
-    <div className="App">
-      <div>{bio}</div>
-    </div>
-  );
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  } else if (!isLoaded) {
+    return <div>Loading...</div>;
+  } else {
+    return (
+      <div>
+        {villagers[index].name}
+        <button
+          onClick={() => {
+            setIndex(index + 1);
+          }}
+        >
+          Next
+        </button>
+      </div>
+    );
+  }
 }
 
 /* <div>{JSON.stringify(villagers)}</div> */
